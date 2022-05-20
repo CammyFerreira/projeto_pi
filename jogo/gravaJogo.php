51ca67e27ff60619f666f0c56b7db90240d2ac39
Oroch Pedro <oliveirarocha_pedro@hotmail.com>
2022-05-20 20:44:36 -0300

    CRUD Pronto

--- jogo/gravaJogo.php
+++ jogo/gravaJogo.php
@@ -12,15 +12,23 @@ if($_FILES['figura']['error'] == 0 &&
 
     $mimeType = mime_content_type($_FILES['figura']['tmp_name']);
 
+        echo "entrou caca";
+
     $campos = explode('/', $mimeType);
     $tipo = $campos[0];
     $ext = $campos[1];
 
-    if($tipo == 'img'){
 
-        $arquivoEnviado = 'img/' . $_FILES['figura']['name'] 
+    
+    var_dump($tipo);
+
+    if($tipo == 'image'){
+
+        $arquivoEnviado = '../img/' . $_FILES['figura']['name'] 
                 . '_' . md5(rand(-9999999, 9999999) . microtime()) 
                     . '.' . $ext;
+        
+                    var_dump($arquivoEnviado);
 
         move_uploaded_file($_FILES['figura']['tmp_name'], "$arquivoEnviado");
     }else{
